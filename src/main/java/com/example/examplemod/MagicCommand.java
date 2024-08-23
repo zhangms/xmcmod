24ebfd74ea2f77b52fe73d6a327f632d4f395971
zms <zms>
2024-08-23 14:49:06 +0800

    magic command

--- src/main/java/com/example/examplemod/MagicCommand.java
+++ src/main/java/com/example/examplemod/MagicCommand.java
@@ -2,8 +2,11 @@ package com.example.examplemod;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import com.mojang.logging.LogUtils;
+import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.world.level.block.Blocks;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
@@ -28,10 +31,25 @@ public class MagicCommand {
                         .executes(context -> {
                             var content = StringArgumentType.getString(context, "content");
                             LOGGER.info("Magic command: {}", content);
+
+                            magic(context, content);
+
                             return Command.SINGLE_SUCCESS;
                         }));
 
         event.getDispatcher().register(magicCommandBuilder);
+    }
+
+    private static void magic(CommandContext<CommandSourceStack> context, String content) {
+        var level = context.getSource().getLevel();
+        var player = context.getSource().getPlayer();
+        if (player == null) {
+            return;
+        }
+        var pos = player.getOnPos();
+        var newPos = pos.east(2).above(2);
+
+        level.setBlock(newPos, Blocks.DIAMOND_BLOCK.defaultBlockState(), 3);
     }
 
 }
